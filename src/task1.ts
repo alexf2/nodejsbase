9f5cd7a59eca9d502d4a51fadac9e3dd446d586c
Alexey Fedorov1 <Alexey_Fedorov1@epam.com>
2021-03-05 12:04:16 +0300

    Реализация Task 1

--- src/task1.ts
+++ src/task1.ts
@@ -1,1 +1,33 @@
-console.log('Hello!');
+import * as readline from 'readline';
+
+const main = () => {
+    const input = process.stdin;
+    const output = process.stdout;
+
+    const rl = readline.createInterface({input, output, terminal: true, prompt: '?>'});
+    rl.write('Введите строку для реверса:\nДля завершения нажмите Ctrl+C\n\n');
+    rl.prompt();
+
+    rl.on('line', line => {
+        if (line) {
+            console.log([...line].reverse().join(''));
+            console.log();
+            // rl.write([...line].reverse().join(''));
+        }
+        rl.prompt();
+    })
+    .on('SIGINT', () => {
+        console.log('!!! closing');
+        rl.close();
+        input.destroy();
+    })
+    .on('close', () => {
+        console.log('!!! closing2');
+        process.exit(0);
+    });
+}
+
+
+main();
+
+// https://stackoverflow.com/questions/20086849/how-to-read-from-stdin-line-by-line-in-node
